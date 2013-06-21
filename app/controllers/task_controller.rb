c3ead33ad9b9545915866d2867b29fbd9df92975
Apprentice <apprentice@devbootcamp.com>
2013-06-21 12:07:31 -0700

    Finished controller methods

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -2,18 +2,35 @@ require_relative '../config/application'
 
 class TaskController < ActiveRecord::Base
   def list
-    Task.all 
+    tasks = Task.all 
+    tasks.each.with_index do |task,i|
+      puts "#{i}. Description: #{task.description} Completed: #{task.completed}"
+    end
   end
 
-  def delete(id)
-
+  def delete(index)
+    tasks.each_with_index do |task, i|
+      if i == index
+        deleted_id = task.id
+      end
+    end
+    end
+    Task.delete(deleted_id)
   end
 
   def create(description)
+    Task.new(description: description)
     
   end
 
-  def complete(id)
-
+  def complete(index)
+    tasks.each_with_index do |task, i|
+      if i == index
+        completed_index = task.id
+      end
+    end
+    Task.update_attributes(complete: true)
   end
 end
+
+
