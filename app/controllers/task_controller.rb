64a85cf1f37aa9bdbe95df25f7c0bc034b686844
Apprentice <apprentice@devbootcamp.com>
2013-06-21 14:59:12 -0700

    refactored task model and controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,7 +1,29 @@
 require_relative '../../config/application'
 
-class TaskController < ActiveRecord::Base
-  
-end
+
+# require 'pathname'
+
+# app_root = Pathname.new(File.expand_path(File.join(File.dirname(__FILE__))))
+# # p app_root.to_s
+# $LOAD_PATH << "#{app_root.to_s}"
+# # p $LOAD_PATH
+# require 'app/controllers/task_controller'
+# require 'app/models/task'
 
 
+p Task.create(description: "buy bananas")
+p Task.create(description: "buy apples")
+p Task.all
+
+p Task.delete_by_index(2)
+p Task.all
+
+p Task.complete_by_index(3)
+p Task.all
+
+
+
+  # tasks.each.with_index do |task,i|
+  #   i += 1
+  #   puts "#{i}. Description: #{task.description} Completed: #{task.complete}"
+  # end
