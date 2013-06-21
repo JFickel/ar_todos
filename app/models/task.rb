a704ff1916a6957f838ca8a4c9420ba6eaac74a6
Apprentice <apprentice@devbootcamp.com>
2013-06-21 15:38:11 -0700

    refactored views and controllers

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,33 +1,23 @@
 require_relative '../../config/application'
 
 class Task < ActiveRecord::Base
-  def self.list
-    tasks = Task.all 
-    tasks.each.with_index do |task,i|
-      i += 1
-      puts "#{i}. Description: #{task.description} Completed: #{task.complete}"
-    end
-  end
-
   def self.delete_by_index(index)
-    tasks = Task.all
-    tasks.each_with_index do |task, i|
-      i += 1
-      deleted_id = task.id if i == index
-      staged_task = task
-    end
-    staged_task.delete(deleted_id)
+    task = stage_task(index)
+    task.delete
   end
 
   def self.complete_by_index(index)
-    tasks = Task.all
-    tasks.each_with_index do |task, i|
-      i += 1
-      completed_index = task.id if i == index
-      staged_task = task
-    end
-    staged_task.update_attributes(complete: true)
+    task = stage_task(index)
+    p task
+    task.update_attributes(complete: true)
   end
 
-  ## Refactor methods for staged task
+  def self.stage_task(index)
+    Task.all.each.with_index do |task, i|
+      i += 1
+      if i == index
+        break task
+      end
+    end
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,10 @@ Dir[APP_ROOT.join('app', 'models', '*.rb')].each do |model_file|
   autoload ActiveSupport::Inflector.camelize(filename), model_file
 end
 
+Dir[APP_ROOT.join('app', 'views', '*.rb')].each do |model_file|
+  filename = File.basename(model_file).gsub('.rb', '')
+  autoload ActiveSupport::Inflector.camelize(filename), model_file
+end
+
 ActiveRecord::Base.establish_connection :adapter  => 'sqlite3',
                                         :database => DB_PATH
